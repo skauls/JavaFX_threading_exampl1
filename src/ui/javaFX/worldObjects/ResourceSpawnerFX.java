76eb9e791c216d3237df92e3c9cbd2cdc40321e9
stevenschwenke <steven@stevenschwenke.de>
2013-01-04 16:16:03 +0100

    Refactoring: MouseOverPane created from ResourceSpawnerFX

--- src/ui/javaFX/worldObjects/ResourceSpawnerFX.java
+++ src/ui/javaFX/worldObjects/ResourceSpawnerFX.java
@@ -1,18 +1,8 @@
 package ui.javaFX.worldObjects;
 
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import ui.javaFX.JavaFxApplication;
 import business.worldObjects.ResourceSpawner;
 
@@ -29,7 +19,7 @@ public class ResourceSpawnerFX extends Button {
 	private ResourceSpawner representedResourceSpawner;
 
 	/** the pane with the context menu */
-	private TitledPane mouseOverPane;
+	private MouseOverPane mouseOverPane;
 
 	public ResourceSpawnerFX(final ResourceSpawner representedResourceSpawner) {
 		super();
@@ -47,11 +37,9 @@ public class ResourceSpawnerFX extends Button {
 					@Override
 					public void handle(MouseEvent event) {
 
-						Parent flowPane = createGridPane();
-
 						if (mouseOverPane == null)
-							mouseOverPane = createMouseOverPane(
-									representedResourceSpawner, flowPane);
+							mouseOverPane = new MouseOverPane(
+									representedResourceSpawner);
 
 						// possible that the pane is already added because the
 						// mouse hovered over this spawner and the pane didn't
@@ -64,76 +52,6 @@ public class ResourceSpawnerFX extends Button {
 					}
 				});
 
-	}
-
-	private TitledPane createMouseOverPane(
-			final ResourceSpawner representedResourceSpawner, Parent flowPane) {
-		final TitledPane mouseOverPane = new TitledPane("RessourceSpawner",
-				flowPane);
-		mouseOverPane.setCollapsible(false);
-		mouseOverPane.setPrefSize(200, 200);
-		mouseOverPane.setLayoutX(representedResourceSpawner.getPosition()
-				.getX());
-		mouseOverPane.setLayoutY(representedResourceSpawner.getPosition()
-				.getY());
-
-		mouseOverPane.addEventFilter(MouseEvent.MOUSE_EXITED,
-				new EventHandler<MouseEvent>() {
-
-					@Override
-					public void handle(MouseEvent event) {
-
-						JavaFxApplication.getInstance().getRootGroup()
-								.getChildren().remove(mouseOverPane);
-					}
-				});
-
-		return mouseOverPane;
-	}
-
-	private GridPane createGridPane() {
-
-		GridPane pane = new GridPane();
-
-		pane.setHgap(4);
-		pane.setVgap(6);
-
-		pane.setPadding(new Insets(5, 3, 3, 3));
-
-		ColumnConstraints colinfo = new ColumnConstraints(20, 70, 90);
-		for (int j = 0; j <= 2; j++) {
-			pane.getColumnConstraints().add(colinfo);
-		}
-
-		ObservableList<Node> content = pane.getChildren();
-
-		Label label = new Label("Resources:");
-		GridPane.setConstraints(label, 0, 0);
-		GridPane.setHalignment(label, HPos.LEFT);
-		content.add(label);
-
-		final Label resourceLabel = new Label(""
-				+ representedResourceSpawner.getResources());
-		GridPane.setConstraints(resourceLabel, 1, 0);
-		GridPane.setHalignment(resourceLabel, HPos.LEFT);
-		content.add(resourceLabel);
-
-		Button addButton = new Button("Add");
-		GridPane.setConstraints(addButton, 2, 0);
-		GridPane.setHalignment(addButton, HPos.RIGHT);
-		content.add(addButton);
-
-		addButton.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				representedResourceSpawner.spawn();
-				resourceLabel.setText(""
-						+ representedResourceSpawner.getResources());
-			}
-		});
-
-		return pane;
 	}
 
 }

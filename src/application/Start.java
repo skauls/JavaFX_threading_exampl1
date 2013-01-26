300e5678dfb2c4346a3de86f420d6c37f033e177
stevenschwenke <steven@stevenschwenke.de>
2013-01-26 13:45:45 +0100

    JavaFX application starts in a new thread which gets terminated after closing the window

--- src/application/Start.java
+++ src/application/Start.java
@@ -3,17 +3,38 @@ package application;
 import ui.javaFX.JavaFxApplication;
 
 /**
- * Entry point for this project. Here, the used UI framework is chosen.
+ * Entry point for this project. Here, the used UI framework is chosen. The
+ * application is started in a new thread.
  * 
  * @author Steven Schwenke
  * 
  */
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		// If there were other user interfaces, here would be the decision which
 		// of them to start. Right now, there is only JavaFX.
-		JavaFxApplication.main(new String[0]);
+
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				System.out.println("Starting JavaFX ...");
+				JavaFxApplication.main(new String[0]);
+				System.out.println("JavaFX ended");
+			}
+		};
+
+		t.start();
+
+		while (true) {
+			if (!t.isAlive()) {
+				Thread.sleep(200);
+				System.out.println("terminating or closing java program");
+				System.exit(1);
+			}
+		}
+
 	}
+
 }
